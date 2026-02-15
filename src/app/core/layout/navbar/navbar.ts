13c315ababb8320d3c85b036f6da15e5b134ea6d
SaadElQuaul <saadelquaul@gmail.com>
2026-02-15 15:02:18 +0000

    add logout functionality and current user retrieval in Navbar component along with the buttons for Jobs and Fav and Jobs Applications ro navigation.

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import {RouterLink, RouterLinkActive} from '@angular/router';
+import { AuthService } from '../../services/authService';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
+  private authService = inject(AuthService);
 
+  logout() {
+    this.authService.logout();
+  }
+
+  currentUser() {
+    return this.authService.currentUser;
+  }
 }
