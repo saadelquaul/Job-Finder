9d2e60415b2012785e80824ac026850aa1e47933
SaadElQuaul <saadelquaul@gmail.com>
2026-02-16 13:57:16 +0000

    add FavoritesList component with template, styles, and unit tests

---
+++ src/app/features/favorites/favorites-list/favorites-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-favorites-list',
+  imports: [],
+  templateUrl: './favorites-list.html',
+  styleUrl: './favorites-list.css',
+})
+export class FavoritesList {
+
+}
