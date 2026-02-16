08c4a4ce83d77ce0930ab8a6ddf3c6f27908a3b6
SaadElQuaul <saadelquaul@gmail.com>
2026-02-16 13:58:33 +0000

    add fields for job details

--- src/app/core/models/job-offer.ts
+++ src/app/core/models/job-offer.ts
@@ -1,3 +1,12 @@
 export interface JobOffer {
-
+slug: string;
+company_name: string;
+title: string;
+description: string;
+remote: boolean;
+url: string;
+tags: string[];
+job_types: string[];
+location: string;
+created_at: number;
 }
