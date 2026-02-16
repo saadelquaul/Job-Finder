d0f44c2a43a51ec9b85ec0f63524050efda4a3e3
SaadElQuaul <saadelquaul@gmail.com>
2026-02-16 13:57:34 +0000

    add JobList component with template, styles, and unit tests

---
+++ src/app/features/jobs/job-list/job-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-list',
+  imports: [],
+  templateUrl: './job-list.html',
+  styleUrl: './job-list.css',
+})
+export class JobList {
+
+}
