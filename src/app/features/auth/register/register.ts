f2eda9a46db8ba43d567505d17ddcca4273b00ef
SaadElQuaul <saadelquaul@gmail.com>
2026-02-22 11:54:03 +0000

    add register component with updated layout and integrate register form

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {RegisterForm} from './register-form/register-form';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RegisterForm],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
