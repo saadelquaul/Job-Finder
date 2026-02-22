2949c1d562bf4a9d84583fb739b716e52791c1ac
SaadElQuaul <saadelquaul@gmail.com>
2026-02-22 11:49:02 +0000

    update login component layout and integrate login form

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {LoginForm} from './login-form/login-form';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginForm],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
