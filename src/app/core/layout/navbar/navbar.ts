5c139affc4d59b4b753d4b04517f5141d65bafea
SaadElQuaul <saadelquaul@gmail.com>
2026-02-23 14:20:05 +0000

    refactor navbar component to remove unused inject import and maintain readability

--- src/app/core/layout/navbar/navbar.ts
+++ src/app/core/layout/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import {Router, RouterLink, RouterLinkActive} from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 @Component({
@@ -22,4 +22,6 @@ export class Navbar {
     this.authService.logout();
     this.router.navigate(['/']);
   }
+
+  protected readonly AuthService = AuthService;
 }
