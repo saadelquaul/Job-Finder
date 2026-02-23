0386ef4cedc5dc2db7201b518aae48130954919d
SaadElQuaul <saadelquaul@gmail.com>
2026-02-23 14:23:50 +0000

    feat: add json-server for mock API, configure NgRx store and effects, and enhance routing
    - Added json-server command to package.json for local API simulation.
    - Configured NgRx store with favorites reducer and effects in app.config.ts.
    - Enhanced app routing to include lazy-loaded routes for jobs, auth, favorites, and applications, with auth guard protection.
    - Created auth.routes.ts for login and registration components.
    - Updated app styles with main content styling.
    - Modified app.spec.ts to reflect changes in the application title.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import { errorInterceptor } from './core/interceptors/error.interceptor-interceptor';
+import { provideStore } from '@ngrx/store';
+import { favoritesReducer } from './store/favorites/favorites.reducer';
+import { provideEffects } from '@ngrx/effects';
+import { FavoritesEffects } from './store/favorites/favorites.effects';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([errorInterceptor])),
+    provideStore({ favorites: favoritesReducer }),
+    provideEffects([FavoritesEffects]),
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./features/jobs/jobs.routes').then((m) => m.JOBS_ROUTES),
+    },
+    {
+        path: '',
+        loadChildren: () => import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+    },
+    {
+        path: 'favorites',
+        canActivate: [authGuard],
+        loadChildren: () =>
+            import('./features/favorites/favorites.routes').then((m) => m.FAVORITES_ROUTES),
+    },
+    {
+        path: 'applications',
+        canActivate: [authGuard],
+        loadChildren: () =>
+            import('./features/applications/applications.routes').then((m) => m.APPLICATIONS_ROUTES),
+    },
+    {
+        path: '**',
+        redirectTo: '',
+    },
+];
